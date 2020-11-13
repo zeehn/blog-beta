089d80ea3cfd73e6b712f253cb28f0f25e3a0207
Zakir Hussain <zack.mesacc@gmail.com>
2020-11-13 19:29:20 +0500

    Add signin on signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update]
-  before_action :require_login, except: [:index, :show]
+  before_action :require_login, except: [:index, :show, :new, :create]
   before_action :require_owner, only: [:edit, :update]
 
   def index
@@ -14,8 +14,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       flash[:success] = "Welcome to the tech blog #{@user.username}"
-      redirect_to articles_path
+      redirect_to @user
     else
       render :new
     end
@@ -34,7 +35,6 @@ class UsersController < ApplicationController
   end
 
   def show
-   
   end
 
   private
